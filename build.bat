8d6dd6a1c389c694fd51035a3897d859df1f9276
Anthony Serravalle <anthony.serravalle@gmail.com>
2023-02-25 08:47:02 -0500

    build

---
+++ build.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set OUT_DIR=.bot
+
+if exist "%OUT_DIR%" rmdir /s/q "%OUT_DIR%"
+mkdir "%OUT_DIR%"
+
+copy .token "%OUT_DIR%"
+copy src\*.py "%OUT_DIR%"
+copy etc\* "%OUT_DIR%"
+
+python -m venv "%OUT_DIR%\.env"
+call "%OUT_DIR%\.env\Scripts\activate.bat"
+pip install -r requirements.txt
+call "%OUT_DIR%\.env\Scripts\deactivate.bat"
+
+set "RUN_SCRIPT=%OUT_DIR%\run.bat"
+echo @echo off> "%RUN_SCRIPT%"
+echo call .env\Scripts\activate.bat>> "%RUN_SCRIPT%"
+echo python bot.py>> "%RUN_SCRIPT%"
+echo call .env\Scripts\deactivate.bat>> "%RUN_SCRIPT%"
---
+++ src/rule.py
@@ -0,0 +1,76 @@
+import json
+import os
+import random
+
+
+class Rules:
+    '''Main entry point for processing a message. The constructor takes the path to the rules file for loading the
+    rule definitions. Otherwise defines a single async 'handle' method, which takes in a Discord message and sends a
+    response to it, if necessary.'''
+
+    def __init__(self, path):
+        self._rules = self._read_rules(path)
+
+    def _read_rules(self, path):
+        config = self._read_json(path)
+        return self._parse_rules(config)
+
+    def _read_json(self, path):
+        if not os.path.isfile(path):
+            raise ValueError(f'No such path: {path}')
+        with open(path, 'r') as f:
+            return json.loads(f.read())
+
+    def _parse_rules(self, config):
+        rules = []
+        for rule_json in config['rules']:
+            rule = Rule(rule_json)
+            rules.append(rule)
+        return rules
+
+    async def handle(self, message):
+        for rule in self._rules:
+            if rule.should_apply(message):
+                response = rule.get_response()
+                if response:
+                    await message.channel.send(response)
+                break
+
+
+class Rule:
+
+    def __init__(self, rule_json):
+        self._channel = rule_json.get('channel', None)
+        self._rate = rule_json.get('rate', 1.0)
+        self._authors = rule_json.get('authors', [])
+        self._substrings = rule_json.get('contains', [])
+        self._excludes = rule_json.get('not_contains', [])
+        self._responses = rule_json.get('responses', [])
+        self._validate()
+    
+    def _validate(self):
+        if self._channel is None:
+            raise Exception('No channel specified')
+        if len(self._responses) == 0:
+            raise Exception('Rule has no responses')
+        if len(self._authors) == 0 and len(self._substrings) == 0 and len(self._excludes) == 0:
+            raise Exception('Rule has no definitions')
+
+    def should_apply(self, message):
+        if message.channel.name != self._channel:
+            return False
+        if len(self._authors) > 0 and message.author.name not in self._authors:
+            return False
+        content = message.content.lower()
+        for substring in self._substrings:
+            if substring not in content:
+                return False
+        for exclude in self._excludes:
+            if exclude in content:
+                return False
+        return True
+
+    def get_response(self):
+        if random.random() < self._rate:
+            return random.choice(self._responses)
+        return ''
