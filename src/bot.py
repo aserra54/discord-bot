5c04639be10a2b91a823328ff32460a27a6248d6
Anthony Serravalle <anthony.serravalle@gmail.com>
2023-02-26 17:05:32 -0500

    doc

--- src/bot.py
+++ src/bot.py
@@ -10,8 +10,8 @@ INTENTS.message_content = True
 
 
 class RedmacBot(commands.Bot):
-    '''The main client for running the bot. Reads the token and parses the rules, the latter of which is used to
-    determine how to respond to users.'''
+    '''The bot itself. Parses the rules, the latter of which is used to determine how to respond to users. It will
+    automatically refresh the rules over time, as they become stale.'''
 
     def __init__(self, rules_path):
         super().__init__(intents=INTENTS, command_prefix='!')
@@ -30,11 +30,15 @@ class RedmacBot(commands.Bot):
 
     def _refresh_rules(self):
         current_time = time.time()
-        if current_time - self._last_read_time > 5:
+        if current_time - self._last_read_time > 60:
+
+            # only refresh the rules if the file has changed
             mod_time = os.path.getmtime(self._rules_path)
             if mod_time < self._last_read_time:
                 print('No updates to rules, skipping')
                 return
+
+            # try refreshing the rules; if they cannot be parsed, then keep the old rules
             print('Refreshing rules')
             try:
                 new_rules = rule.Rules(self._rules_path)
