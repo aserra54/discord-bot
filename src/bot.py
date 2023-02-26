0b8d05b1d44f9157d0799b45b383133f2ef7397d
Anthony Serravalle <anthony.serravalle@gmail.com>
2023-02-26 12:33:49 -0500

    feat: refactored client into its own class (#8)
    Refactored the bot client into its own class to better encapsulate its behavior. The only remaining oddity is the `token`, which is read by the client and also passed into the client's `run` method; the behavior has been left as-is for now, but could change in the future.

--- src/bot.py
+++ src/bot.py
@@ -1,43 +1,11 @@
-import discord
-import os
-import rule
+import client
 
 
-rules = None
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
-
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    await rules.handle(message)
-
-
-def start_client():
-    global rules
-    token = read_token('.token')
-    rules = read_rules()
-    client.run(token)
-
-
-def read_token(path):
-    if not os.path.isfile(path):
-        raise ValueError(f'No such file: {path}')
-    with open(path, 'r') as f:
-        return f.read()
-
-
-def read_rules():
-    return rule.Rules('rules.json')
+def main():
+    bot_client = client.RedmacBotClient('.token', 'rules.json')
+    bot_token = bot_client.token
+    bot_client.run(bot_token)
 
 
 if __name__ == '__main__':
-    start_client()
+    main()
---
+++ src/client.py
@@ -0,0 +1,29 @@
+import discord
+import os
+import rule
+
+
+class RedmacBotClient(discord.Client):
+    '''The main client for running the bot. Reads the token and parses the rules, the latter of which is used to
+    determine how to respond to users.'''
+
+    def __init__(self, token_path, rules_path):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        super().__init__(intents=intents)
+        self.token = self._read_token(token_path)
+        self._rules = rule.Rules(rules_path)
+    
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        await self._rules.handle(message)
+
+    def _read_token(self, path):
+        if not os.path.isfile(path):
+            raise ValueError(f'No such file: {path}')
+        with open(path, 'r') as f:
+            return f.read()
