16316b395970c3542a2064fde82205ae73322b6b
Anthony Serravalle <anthony.serravalle@gmail.com>
2023-02-26 12:30:35 -0500

    refactor

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,11 @@
 import client
 
 
-if __name__ == '__main__':
+def main():
     bot_client = client.RedmacBotClient('.token', 'rules.json')
     bot_token = bot_client.token
     bot_client.run(bot_token)
+
+
+if __name__ == '__main__':
+    main()
