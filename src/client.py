a681e5f5a64bcdfdd6c2c11aac685da8ec4060b3
Anthony Serravalle <anthony.serravalle@gmail.com>
2023-02-26 16:53:46 -0500

    auto read

--- src/client.py
+++ src/client.py
@@ -1,5 +1,6 @@
 import discord
 import rule
+import time
 from discord.ext import commands
 
 
@@ -13,12 +14,26 @@ class RedmacBotClient(commands.Bot):
 
     def __init__(self, rules_path):
         super().__init__(intents=INTENTS, command_prefix='!')
+        self._rules_path = rules_path
         self._rules = rule.Rules(rules_path)
+        self._last_read_time = time.time()
     
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
 
     async def on_message(self, message):
+        self._refresh_rules()
         if message.author == self.user:
             return
         await self._rules.handle(message)
+
+    def _refresh_rules(self):
+        if time.time() - self._last_read_time > 60:
+            print('Refreshing rules')
+            try:
+                new_rules = rule.Rules(self._rules_path)
+            except:
+                print('Failed to read rules')
+                return
+            self._rules = new_rules
+            self._last_read_time = time.time()
