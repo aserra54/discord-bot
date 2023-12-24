c9eed3b52651cb87c5c5c20949eab632a9cc6d79
Anthony Serravalle <anthony.serravalle@gmail.com>
2023-12-23 21:48:47 -0500

    feat: added support for side missions (#16)
    Adds support for side missions when using the `/drg` command. Also
    greatly simplifies the rules to no longer pick on users.

--- src/main.py
+++ src/main.py
@@ -29,11 +29,13 @@ async def drg(
     complexity: int = commands.Param(choices=[1, 2, 3]),
     location: str = commands.Param(choices=['Azure Weald', 'Crystalline Caverns', 'Dense Biozone', 'Fungus Bogs', 'Glacial Strata', 'Hollow Bough', 'Magma Core', 'Radioactive Exclusion Zone', 'Salt Pits', 'Sandblasted Corridors']),
     mission_type: str = commands.Param(choices=['Deep Dive', 'Egg Hunt', 'Elimination', 'Escort Duty', 'Industrial Sabotage', 'Mining Expedition', 'On-site Refining', 'Point Extraction', 'Salvage Operation']),
+    side_mission: str = commands.Param(choices=['Alien Fossils', 'Apoca Blooms', 'Boolo Caps', 'Dystrum', 'Ebonuts', 'Fester Fleas', 'Gunk Seeds', 'Hollomite']),
     anomaly: str = commands.Param(choices=['-', 'Critical Weakness', 'Double XP', 'Gold Rush', 'Golden Bugs', 'Low Gravity', 'Mineral Mania', 'Rich Atmosphere', 'Volatile Guts']),
     mutator1: str = commands.Param(choices=['-', 'Cave Leech Cluster', 'Elite Threat', 'Exploder Infestation!', 'Haunted Cave', 'Lethal Enemies', 'Lithophage Outbreak', 'Low Oxygen', 'Mactera Plague', 'Parasites', 'Regenerative Bugs', 'Rival Presence', 'Shield Disruption', 'Swarmageddon']),
     mutator2: str = commands.Param(choices=['-', 'Cave Leech Cluster', 'Elite Threat', 'Exploder Infestation!', 'Haunted Cave', 'Lethal Enemies', 'Lithophage Outbreak', 'Low Oxygen', 'Mactera Plague', 'Parasites', 'Regenerative Bugs', 'Rival Presence', 'Shield Disruption', 'Swarmageddon']),
+    game_type: str = commands.Param(choices=['Solo', 'Multiplayer']),
 ):
-    await app.command_handler.handle_drg(interaction, length, complexity, location, mission_type, anomaly, mutator1, mutator2)
+    await app.command_handler.handle_drg(interaction, length, complexity, location, mission_type, side_mission, anomaly, mutator1, mutator2, game_type)
 
 
 def read_token(token_path):
