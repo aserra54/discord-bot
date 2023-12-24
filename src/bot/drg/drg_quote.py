7535f9db3f7e798088e166afdcf63d0478e30ef7
Anthony Serravalle <anthony.serravalle@gmail.com>
2023-12-24 00:20:19 -0500

    added description, more quotes

--- src/bot/drg/drg_quote.py
+++ src/bot/drg/drg_quote.py
@@ -9,10 +9,16 @@ quotes = [
     ("Help! I'm stuck in the ceiling!", 'Dwarf', 'when grabbed by a cave leech'),
     ('From A to D, skipping B and C!', 'Scout', 'when using the grappling hook'),
     ('Ready to blow!', 'Driller', 'when placing a satchel charge'),
+    ('I call it my "grenade-grenade"!', 'Gunner', 'when throwing a cluster grenade'),
+    ('Multi-boom!', 'Gunner', 'when throwing a cluster grenade'),
+    ("Turrets and explosions! You've come to the right place, mate.", 'Engineer', 'when selected'),
+    ('Two-thousand rounds of depleted uranium, aw yeah!', 'Gunner', 'when selected'),
+    ('Allow me to "illuminate" the situation! Hahahaha!', 'Scout', 'when selected'),
+    ("If it ain't drillable, it's probably flammable.", 'Driller', 'when selected'),
 ]
 
 
-@bot.slash_command(name='drg-quote')
+@bot.slash_command(name='drg-quote', description='Returns a random quote from Deep Rock Galactic.')
 async def drg_quote(self, interaction: ApplicationCommandInteraction):
     await DrgQuoteCommand().handle(interaction)
 
